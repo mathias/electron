c26aadbdc56a3958f389989dacd74f64f960d199
Matt Gauger <matt.gauger@gmail.com>
2010-10-16 19:35:51 -0500

    Add Projects model and simple CRUD operations.

---
+++ app/models/project.rb
@@ -0,0 +1,46 @@
+class Project < Ohm::Model
+  extend Spawn
+  
+  attribute :name
+  attribute :link
+  attribute :description
+  attribute :datetime
+  attribute :published
+  
+  def validate
+    assert_present(:name)
+    assert_present(:link)
+    assert_present(:description)
+    assert_numeric(:published)
+  end
+  
+  def create
+    self.datetime ||= Time.now.strftime("%Y-%m-%d %H:%M:%S")
+    self.published = 0 # Set unpublished by default
+    super
+  end
+  
+  def to_s
+    name.to_s
+  end
+  
+  def to_param
+    name
+  end
+  
+  def published?
+    if (published.to_i==1)
+      return "Published"
+    else
+      return "Unpublished"
+    end
+  end
+
+  
+  protected
+  
+  spawner do |project|
+    project.name = Faker::Company.catch_phrase
+    project.link = Fake::Internet.domain_name
+  end
+end
---
+++ app/routes/projects.rb
@@ -0,0 +1,43 @@
+class Main
+  get "/projects/new" do
+    @project = Project.new
+    haml :"projects/new"
+  end
+  
+  get "/projects/:id" do
+    @project = Project[params[:id]]
+    @url = @project.link
+    haml :"projects/id"
+  end
+  
+  post "/projects" do
+    @project = Project.new(params[:project])
+    
+    if @project.valid?
+      @project.create
+      session[:notice] = "Your project has been added"
+      redirect "/projects"
+    else
+      haml :"projects/new"
+    end
+  end
+
+  get "/projects" do
+    @projects = Project.all
+    haml :"projects/list"
+  end
+  
+  get "/projects/delete/:id" do
+    @project = Project[params[:id]]
+    @project.delete
+    redirect "/projects"
+  end
+  
+
+  get "/projects/publish/:id" do
+    @project = Project[params[:id]]
+    @project.published = 1
+    @project.save
+    redirect "/projects"
+  end
+end
