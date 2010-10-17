3cfb225130d312cc73dc544b0f493cebfad62b4d
Matt Gauger <matt.gauger@gmail.com>
2010-10-16 19:35:25 -0500

    Add a scenario for listing projects, which breaks initially.

--- test/stories/site_test.rb
+++ test/stories/site_test.rb
@@ -1,11 +1,17 @@
 require "stories_helper"
 
 class SiteTest < Test::Unit::TestCase
-  story "As a developer I want to see the homepage so I know Monk is correctly installed" do
+  story "As a user I want to see the homepage" do
     scenario "A visitor goes to the homepage" do
       visit "/"
 
       assert_contain "Hello, world!"
+    end
+
+    scenario "A visitor goes to the projects list" do
+      visit "/projects"
+      
+      assert_contain "Projects"
     end
   end
 end
